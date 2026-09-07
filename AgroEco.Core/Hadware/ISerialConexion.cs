d50820a7d0c8d0da83c922e4f7f305d84f6553cd
rtnwe <robertojoseromero7@Gmail.com>
2026-09-07 09:56:03 -0600

    feat(jobs): add action/trigger types, factories and hardware activation action

---
+++ AgroEco.Core/Hadware/ISerialConexion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgroEco.Core.Hadware
+{
+    public interface ISerialConexion
+    {
+        bool connected { get; }
+        Task<Result> OpenConnexion();
+        Task<Result> CloseConexion();
+
+    }
+}
