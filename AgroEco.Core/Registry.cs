f2432fd5da87b82c2056656f377600caae20d89d
rtnwe <robertojoseromero7@Gmail.com>
2026-09-15 15:20:30 -0600

    feat: add generic ConcurrentDictionary registry implementation

---
+++ AgroEco.Core/Registry.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Concurrent;
+
+public class Registry<TKey, TValue> where TValue : class
+{
+    private readonly ConcurrentDictionary<TKey, TValue> _items = new();
+
+    public void Register(TKey key, TValue value)
+        => _items[key] = value;
+
+    public bool Unregister(TKey key)
+        => _items.TryRemove(key, out _);
+
+    public TValue? Get(TKey key)
+        => _items.TryGetValue(key, out var v) ? v : null;
+
+    public IEnumerable<TValue> All()
+        => _items.Values;
+
+    public IEnumerable<TValue> Where(Func<TValue, bool> predicate)
+        => _items.Values.Where(predicate);
+}
